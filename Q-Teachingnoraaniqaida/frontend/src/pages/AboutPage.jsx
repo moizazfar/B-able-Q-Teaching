28aa7b2ff846f4cf8a14fbe4d141443885f45c7f
Moiz Azfar <122431796+moizazfar@users.noreply.github.com>
2024-08-17 23:04:37 +0500

    Update AboutPage.jsx

--- Q-Teachingnoraaniqaida/frontend/src/pages/AboutPage.jsx
+++ Q-Teachingnoraaniqaida/frontend/src/pages/AboutPage.jsx
@@ -4,35 +4,41 @@ import { styled } from "@mui/system";
 import girlimg from "../Assets/image/image 2.png";
 import boyimg from "../Assets/image/image 3.png";
 
-const AboutImage = styled("img")({
+const AboutImage = styled("img")(({ theme }) => ({
   maxWidth: "100%",
   height: "auto",
-});
+  [theme.breakpoints.down("sm")]: {
+    minHeight: "20vh",
+    // height: "10%",
+  },
+}));
 
 const AboutPage = () => {
   return (
     <Box
       sx={{
         width: "100vw",
-        height: "100vh",
+        // height: "85vh",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
         bgcolor: "#f5f5f5",
+        minHeight: { xs: "20vh", md: "85vh", lg: "85vh" },
       }}
     >
       <Container sx={{ textAlign: "center", width: "75vw" }}>
         <Grid container spacing={2} justifyContent="center" alignItems="center">
-          <Grid item xs={12} sm={4}>
+          <Grid item xs={2.5} sm={4}>
             <AboutImage src={girlimg} alt="Girl" loading="lazy" />
           </Grid>
-          <Grid item xs={12} sm={4}>
+          <Grid item xs={7} sm={4}>
             <Typography
               variant="h1"
               sx={{
                 fontFamily: "Kaushan Script",
-                fontSize: 60,
+                fontSize: { xs: 30, md: 60 },
                 color: "#10183D",
+                mb: 2,
               }}
             >
               About Us
@@ -44,6 +50,7 @@ const AboutPage = () => {
                 color: "#DCBA84",
                 mt: 2,
                 fontWeight: "800",
+                fontSize: { xs: 13, md: 17 },
               }}
             >
               We are providing online Quran classes that are suitable for the
@@ -58,7 +65,7 @@ const AboutPage = () => {
               experience it for yourself.
             </Typography>
           </Grid>
-          <Grid item xs={12} sm={4}>
+          <Grid item xs={2.5} sm={4}>
             <AboutImage src={boyimg} alt="Boy" loading="lazy" />
           </Grid>
         </Grid>
