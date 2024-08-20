5e0f22023c3ff69849b17b591c39459ab87ba4e6
Moiz Azfar <122431796+moizazfar@users.noreply.github.com>
2024-08-21 03:01:07 +0500

    Update views.py

--- Q-Teachingnoraaniqaida/backend/signrecognition/views.py
+++ Q-Teachingnoraaniqaida/backend/signrecognition/views.py
@@ -19,8 +19,42 @@ def get_csrf_token(request):
     return JsonResponse({"csrfToken": token})
 
 
-def recognize_sign_api(image):
-    prediction_letter = "ALIF"
+HUROOF_DICT = {
+    0: "ALIF",
+    1: "BAA",
+    2: "TAA",
+    3: "SAA",
+    4: "JEEM",
+    5: "HAA",
+    6: "KHA",
+    7: "DAAL",
+    8: "ZAAL",
+    9: "RAA",
+    10: "ZAA",
+    11: "SEEN",
+    12: "SHEEN",
+    13: "SUAD",
+    14: "ZUAD",
+    15: "TAU",
+    16: "ZAU",
+    17: "AEEEN",
+    18: "GAEEN",
+    19: "FAA",
+    20: "QAAF",
+    21: "KAAF",
+    22: "LAAM",
+    23: "MEEM",
+    24: "NOON",
+    25: "WOW",
+    26: "HAMZA",
+    27: "HAA1",
+    28: "BARI_YA",
+    29: "CHOTI_YAA",
+}
+
+
+def recognize_sign_api(image, huroof_id):
+    prediction_letter = HUROOF_DICT.get(huroof_id)
     confidence_score = random.randint(50, 100)
     return prediction_letter, confidence_score
 
@@ -29,7 +63,13 @@ def recognize_sign_api(image):
 @permission_classes([IsAuthenticated])
 def recognize_sign(request):
     image = request.FILES.get("image")
-    prediction_letter, confidence_score = recognize_sign_api(image)
+    huroof_id = int(request.data.get("huroof_id"))
+
+    if huroof_id not in HUROOF_DICT:
+        return JsonResponse({"error": "Invalid Huroof ID"}, status=400)
+
+    prediction_letter, confidence_score = recognize_sign_api(image, huroof_id)
+
     return JsonResponse(
         {"prediction_letter": prediction_letter, "confidence_score": confidence_score}
     )
